06c81b71af8e7e8943d6016ce5d5a6446d760b66
everjheyco-art <everjheyco@gmail.com>
2026-05-25 16:40:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// Crear corazones al cargar
+const colores = ['#ff0055', '#990099', '#ff3366', '#800080'];
 const contenedor = document.getElementById('corazones');
-for(let i = 0; i < 40; i++) { 
+
+for(let i = 0; i < 30; i++) {
     let c = document.createElement('div');
     c.innerHTML = '❤️';
     c.className = 'corazon';
     c.style.left = Math.random() * 100 + '%';
-    c.style.animationDuration = (Math.random() * 5 + 5) + 's';
-    c.style.animationDelay = Math.random() * 5 + 's';
+    c.style.top = Math.random() * 100 + '%';
+    c.style.color = colores[Math.floor(Math.random() * colores.length)];
     contenedor.appendChild(c);
 }
 
-// Función del botón
 function iniciarExperiencia() {
     document.getElementById("audioMusica").play();
     document.getElementById('btn-reproducir').style.display = 'none';
